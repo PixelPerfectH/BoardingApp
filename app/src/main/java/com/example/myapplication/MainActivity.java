cf5fa6503caf040ef38b28f46c063156fbd6ca46
rodion56730 <83105916+rodion56730@users.noreply.github.com>
2023-03-29 23:41:54 +0300

    error

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,12 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView loginTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activty_main);
-        loginTextView = findViewById(R.id.LoginTextTV);
+        TextView loginTextView = findViewById(R.id.LoginTextTV);
         String login = getIntent().getExtras().get("1").toString();
         loginTextView.setText(login);
 
