a8f1ccffc51dd7413545d4956c6931ee5e738be1
rodion56730 <83105916+rodion56730@users.noreply.github.com>
2023-03-30 14:42:07 +0300

    1.3

---
+++ app/src/main/java/com/example/myapplication/TaskAdapter.java
@@ -0,0 +1,82 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
+    Context context;
+    private final List<Task> mData;
+    public final List<Task> FullData;
+    private final TaskListener listener;
+    @NonNull
+    @Override
+    public TaskAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        context = parent.getContext();
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskAdapter.ViewHolder holder, int position) {
+        Task task = mData.get(position);
+        holder.bindView(task);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void removeTask(int pos) {
+        Task task = mData.get(pos);
+        mData.remove(task);
+        FullData.remove(task);
+        notifyItemRemoved(pos);
+    }
+
+    public TaskAdapter(List<Task> mData, TaskListener taskListener){
+        FullData = mData;
+        this.mData = new ArrayList<>(mData);
+        this.listener = taskListener;
+    }
+
+
+    public Task getTask(int position){
+        return FullData.get(position);
+    }
+    public interface TaskListener{
+        void onTaskClick(View v, int position);
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public Task task;
+        final TextView name,description;
+        final ImageView avatar;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.textViewNameItem);
+            description = itemView.findViewById(R.id.textViewItem);
+            avatar = itemView.findViewById(R.id.imageViewItem);
+            itemView.setOnClickListener(v -> listener.onTaskClick(v, getAdapterPosition()));
+
+        }
+
+        void bindView(Task task) {
+            this.task = task;
+            name.setText(task.getName());
+            description.setText(task.getDescription());
+            avatar.setImageBitmap(task.getImage());
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/TasksActivity.java
+++ app/src/main/java/com/example/myapplication/TasksActivity.java
@@ -1,4 +1,46 @@
 package com.example.myapplication;
 
-public class TasksActivity {
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TasksActivity extends AppCompatActivity {
+
+        private RecyclerView recyclerView;
+        private TaskAdapter taskAdapter;
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_tasks);
+            recyclerView = findViewById(R.id.recyclerView_tasks);
+
+            ItemTouchHelper.SimpleCallback callback = new SwipeItem(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
+            new ItemTouchHelper(callback).attachToRecyclerView(recyclerView);
+
+        }
+
+
+
+    class SwipeItem extends ItemTouchHelper.SimpleCallback {
+
+        public SwipeItem(int dragDirs, int swipeDirs) {
+            super(dragDirs, swipeDirs);
+        }
+
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            if (viewHolder instanceof TaskAdapter.ViewHolder) {
+                //метод свайпа должен удалять выполненную задачу из списка, Коля это тебе
+                taskAdapter.removeTask(viewHolder.getAdapterPosition());
+            }
+        }
+    }
 }
