bc6a5ddeedd3245d6b8e6344ea34b20958ed9598
CleroStyle <glazacevnikolaj7@gmail.com>
2023-04-02 01:17:59 +0300

    Update events

--- app/src/main/java/com/example/myapplication/Events/EventsActivity.java
+++ app/src/main/java/com/example/myapplication/Events/EventsActivity.java
@@ -12,9 +12,13 @@ import android.view.View;
 
 import com.example.myapplication.R;
 import com.example.myapplication.models.Description;
+import com.example.myapplication.requests.GetRequest;
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class EventsActivity extends Fragment {
 
@@ -23,27 +27,27 @@ public class EventsActivity extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         RecyclerView recyclerView = view.findViewById(R.id.recycler);
         Intent intent = new Intent(getActivity(), Description.class);
         //Заполнить массив events
-        EventAdapter.OnEventClickListener eventClickListener = new EventAdapter.OnEventClickListener() {
-            @Override
-            public void onEventClick(Event state, int position) {
-                intent.putExtra("eventName", events.get(position).getName());
-                intent.putExtra("eventDescription", events.get(position).getDescription());
-                startActivity(intent);
-            }
+        GetRequest getRequest = new GetRequest();
+        String result;
+        try {
+            result = getRequest.execute("https://clerostyle.drawy.ru/api/event/getlist?userName=" + "CleroStyle" + "&firstElement=0&lastElement=4").get();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (result != null) {
+            Gson gson = new Gson();
+            events = gson.fromJson(result, ArrayList.class);
+        }
+        EventAdapter.OnEventClickListener eventClickListener = (state, position) -> {
+            intent.putExtra("eventName", events.get(position).getName());
+            intent.putExtra("eventDescription", events.get(position).getDescription());
+            startActivity(intent);
         };
         EventAdapter adapter = new EventAdapter(getActivity(), events,eventClickListener);
         recyclerView.setAdapter(adapter);
     }
-
-    private void setInitialData() {
-//        events.add(new Event ("Бразилия", "Бразилиа", new Date()));
-//        events.add(new Event ("Аргентина", "Буэнос-Айрес", new Date()));
-//        events.add(new Event ("Колумбия", "Богота", new Date()));
-//        events.add(new Event ("Уругвай", "Монтевидео",new Date()));
-//        events.add(new Event ("Чили", "Сантьяго", new Date()));
-    }
-
 }
