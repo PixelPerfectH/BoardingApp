c1a2cdec29a83b4b7c12889546b8d524ecb5a209
CleroStyle <glazacevnikolaj7@gmail.com>
2023-04-02 03:08:36 +0300

    Update top

--- app/src/main/java/com/example/myapplication/Profile/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile/Profile.java
@@ -8,30 +8,45 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.Events.Event;
 import com.example.myapplication.R;
+import com.example.myapplication.requests.GetRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Profile extends Fragment {
-    ArrayList<Employee> employees=new ArrayList<>();
+    ArrayList<Employee> employees = new ArrayList<>();
 
-    public Profile(){super(R.layout.activity_user_profile);}
+    public Profile() {
+        super(R.layout.activity_user_profile);
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         employees.clear();
         // начальная инициализация списка
-        setInitialData();
+        GetRequest getRequest = new GetRequest();
+        String result;
+        try {
+            result = getRequest.execute("https://clerostyle.drawy.ru/api/point/gettopusers?userName=" + "CleroStyle" + "&amount=3").get();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if (result != null) {
+            Gson gson = new Gson();
+            employees = gson.fromJson(result, new TypeToken<List<Employee>>(){}.getType());
+        }
         RecyclerView recyclerView = view.findViewById(R.id.three);
         // создаем адаптер
         EmployeeAdapter adapter = new EmployeeAdapter(getActivity(), employees);
         // устанавливаем для списка адаптер
         recyclerView.setAdapter(adapter);
 
-    }
-    private void setInitialData() {
-        employees.add(new Employee ("aaaaaaaa", 1));
-        employees.add(new Employee ("bbbbbbbbb", 2));
-        employees.add(new Employee ("ccccccc", 3));
     }
 }
