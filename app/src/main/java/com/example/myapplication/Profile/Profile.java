9c1530a4518011307a0852b21b01de257edb6830
rodion56730 <83105916+rodion56730@users.noreply.github.com>
2023-04-02 05:19:01 +0300

    Events done!

--- app/src/main/java/com/example/myapplication/Profile/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile/Profile.java
@@ -1,7 +1,9 @@
 package com.example.myapplication.Profile;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,31 +21,38 @@ import java.util.concurrent.ExecutionException;
 
 public class Profile extends Fragment {
     ArrayList<Employee> employees = new ArrayList<>();
+    TextView yourPlaceTV;
+    TextView nameTV;
+    String login;
 
     public Profile() {
         super(R.layout.activity_user_profile);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        employees.clear();
-        // начальная инициализация списка
         GetRequest getRequest = new GetRequest();
+        yourPlaceTV = view.findViewById(R.id.place);
+        nameTV = view.findViewById(R.id.name);
         String result;
+        login = requireActivity().getIntent().getExtras().get("login").toString();
         try {
-            result = getRequest.execute("https://clerostyle.drawy.ru/api/point/gettopusers?userName=" + "CleroStyle" + "&amount=3").get();
+            result = getRequest.execute("https://clerostyle.drawy.ru/api/point/gettopusers?userName=" + login + "&amount=3").get();
         } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
         }
         if (result != null) {
             Gson gson = new Gson();
-            employees = gson.fromJson(result, new TypeToken<List<Employee>>(){}.getType());
+            employees = gson.fromJson(result, new TypeToken<List<Employee>>() {
+            }.getType());
         }
         RecyclerView recyclerView = view.findViewById(R.id.three);
-        // создаем адаптер
+        yourPlaceTV.setText(Integer.toString(employees.get(employees.size() - 1).getPlace()));
+        nameTV.setText(employees.get(employees.size() - 1).getName());
+        employees.remove(employees.size() - 1);
         EmployeeAdapter adapter = new EmployeeAdapter(getActivity(), employees);
-        // устанавливаем для списка адаптер
         recyclerView.setAdapter(adapter);
 
     }
